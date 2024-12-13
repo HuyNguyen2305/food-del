cd7f9d1ec0adadb612cc5dfdbf1aea82d0f895ed
HuyNguyen2305 <126552294+HuyNguyen2305@users.noreply.github.com>
2024-12-13 16:10:50 +0700

    update 16:10 241213

---
+++ admin/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import Order from './pages/Order/Order'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <hr/>
+      <div className="add-content">
+          <Sidebar/>
+          <Routes>
+            <Route path="/add" element={<Add/>}/>
+            <Route path="/list" element={<List/>}/>
+            <Route path="/order" element={<Order/>}/>
+          </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
