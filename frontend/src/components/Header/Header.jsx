16dcfb31182ad8b62fcb25876f99a3699a5c66ed
HuyNguyen2305 <126552294+HuyNguyen2305@users.noreply.github.com>
2024-12-14 14:46:17 +0700

    update 14:46 241214

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,8 +5,6 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
-            <h2>Order your favorite food here!</h2>
-            <p>Choose from the menu</p>
             <button>View Menu</button>
         </div>
     </div>
