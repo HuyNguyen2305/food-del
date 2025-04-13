27468d74ca72a8cf9ce5a98073ec2667adc27d95
HuyNguyen2305 <126552294+HuyNguyen2305@users.noreply.github.com>
2025-04-13 19:06:17 +0700

    update 250413

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import userRouter from "./routes/userRoute.js"
+import foodRouter from "./routes/foodRoute.js"
 import 'dotenv/config'
 
 
@@ -18,7 +19,10 @@ connectDB();
 
 
 //api endpoints
+app.use("/api/food", foodRouter)
+app.use("/images", express.static("uploads")) // for images
 app.use("/api/user", userRouter)
+
 
 
 app.get("/",(req,res)=>{
