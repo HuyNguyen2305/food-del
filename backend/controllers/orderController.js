72894a129bc592d46ce862fc342be2adab47a203
HuyNguyen2305 <126552294+HuyNguyen2305@users.noreply.github.com>
2025-05-11 15:54:48 +0700

    update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,3 +1,4 @@
+import { log } from "console";
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import crypto from 'crypto';
@@ -70,4 +71,54 @@ const placeOrder = async (req, res) => {
     }
 }
 
-export { placeOrder };
+const verifyOrder = async (req, res) =>{
+    const {orderId, success} = req.body;
+    try {
+        if (success == "true"){
+            await orderModel.findByIdAndUpdate(orderId, {payment:true});
+            res.json({success:true, message:"Payment successful"});
+        }
+        else{
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({success:false, message:"Payment failed"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Error in verifying order"});
+    }
+}
+
+// user orders for frontend
+const userOrders = async (req,res) =>{
+    try {
+        const orders = await orderModel.find({userId:req.body.userId});
+        res.json({success:true, data:orders});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Error in fetching orders"});
+    }
+}
+
+// Listing orders for admin
+const listOrders = async(req, res) =>{
+    try {
+        const orders = await orderModel.find();
+        res.json({success:true, data:orders});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Error in fetching orders"});
+    }
+}
+
+//api for updating order status
+const updateStatus = async (req, res) =>{
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, {status:req.body.status});
+        res.json({success:true, message:"Order status updated"});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Error in updating order status"});
+    }
+}
+
+export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus };
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,8 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import './PlaceOrder.css';
 import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
 
 const PlaceOrder = () => {
 
@@ -32,8 +34,31 @@ const PlaceOrder = () => {
         orderItems.push(itemInfo);
       }
     })
-    console.log(orderItems);
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount()+2,
+    }
+    let response = await axios.post(url+"/api/order/place", orderData,{headers:{token}});
+    if (response.data.success){
+      const {session_url} = response.data;
+      window.location.replace(session_url);
+    }
+    else{
+      alert("Error in placing order");
+    }
   }
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token){
+      navigate('/cart');
+    }
+    else if(getTotalCartAmount()===0){
+      navigate('/cart');
+    }
+  },[token])
 
   return (
     <form onSubmit={placeOrder} className='place-order'>
